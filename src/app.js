3d41d4e3485e4eeeee047b799062df4a0ac31b18
Jean Olmedillo <78714438+Jmofuture@users.noreply.github.com>
2024-09-11 23:21:38 +0000

    Generador de excusas

--- src/app.js
+++ src/app.js
@@ -8,4 +8,26 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  generateExcuse();
 };
+
+function generateExcuse() {
+  let who = ["El perro", "Mi abuela", "El cartero", "Mi pájaro"];
+  let action = ["se comió", "orinó", "aplastó", "rompió"];
+  let what = ["mi tarea", "mi teléfono", "el coche"];
+  let when = [
+    "antes de la clase",
+    "mientras dormía",
+    "mientras hacía ejercicio",
+    "durante mi almuerzo",
+    "mientras rezaba"
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  let excuse = `${who[whoIndex]} ${action[actionIndex]} ${what[whatIndex]} ${when[whenIndex]}.`;
+  document.getElementById("excuse").innerHTML = excuse;
+}
